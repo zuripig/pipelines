18de1a99b336b4665becdb8d636bb1f88d358878
zuripig <tom@glanalaw.co.uk>
2021-11-30 10:29:53 +0000

    .

--- jobs/test.groovy
+++ jobs/test.groovy
@@ -5,44 +5,21 @@
 // }
 
 
-// pipelineJob("$basePath/dev/admin") { 
-//     properties {
-//         disableConcurrentBuilds()
-//     }
-
-//     environmentVariables {
-//         env('APP', 'admin')
-//         env('REPO', 'apex-suite-core')
-//         env('DEPLOY_ACCOUNT_ID', "156498023621")
-//         env('BRANCH', 'master')
-//     }
-
-//     logRotator {
-//         numToKeep(5)
-//     }
-    
-//     definition {
-//         cpsScm {
-//             scm {
-//                 git('git@github.com:nexuspointltd/nexuspoint-pipelines.git', 'master')
-//             }
-//             scriptPath("apex-tenants/Jenkinsfile")
-//         }
-//     }
-// }
-
-multibranchPipelineJob('pipeline') {
-    branchSources {
-        git {
-            // id('123456789') // IMPORTANT: use a constant and unique identifier
-            remote('https://github.com/zuripig/pipelines.git')
-            // credentialsId('github2')
-            // includes('JENKINS-*')
-        }
+pipelineJob(seedTest/testPipe) { 
+    properties {
+        disableConcurrentBuilds()
     }
-    orphanedItemStrategy {
-        discardOldItems {
-            numToKeep(20)
+
+    logRotator {
+        numToKeep(5)
+    }
+    
+    definition {
+        cpsScm {
+            scm {
+                git('git@github.com:zuripig/pipelines.git', 'mymain')
+            }
+            scriptPath("jobs/*.groovy")
         }
     }
 }
