ea77b5b48c31eacf80bed5a70af851fa2b0ff3d5
zuripig <tom@glanalaw.co.uk>
2021-12-13 11:01:48 +0000

    .

--- jobs/script.groovy
+++ jobs/script.groovy
@@ -1,10 +1,10 @@
 def names = [
-	'nuxt/node12': ['branch': 'mymain'],
-	'nuxt/nod14': ['branch': 'mymain'],
-    'nuxt/node16': ['branch': 'mymain'],
+	//'nuxt/node12': ['branch': 'mymain'],
+	//'nuxt/nod14': ['branch': 'mymain'],
+    //'nuxt/node16': ['branch': 'mymain'],
     'jenkins': ['branch': 'mymain'],
-    'laravel/php72': ['branch': 'mymain'],
-    'laravel/php74': ['branch': 'mymain'],
+    //'laravel/php72': ['branch': 'mymain'],
+    //'laravel/php74': ['branch': 'mymain'],
     'nginx-proxy': ['branch': 'mymain']
 
 ]
